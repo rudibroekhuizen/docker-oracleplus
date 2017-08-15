72ffd0a413af08d5ba445152dc108b12c274540b
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-08-15 11:14:02 +0200

    Update sqlarea.sql

--- sqlcl/scripts/sqlarea.sql
+++ sqlcl/scripts/sqlarea.sql
@@ -1,2 +1,30 @@
-select /*json*/ * from v$sqlarea;
+select /*json*/ 
+sql_text,
+sql_id,
+sharable_mem
+persistent_mem,
+runtime_mem,
+sorts,
+version_count,
+loaded_versions,
+open_versions,
+users_opening,
+fetches,
+executions,
+users_executing,
+loads,
+first_load_time,
+parse_calls,
+disk_reads,
+buffer_gets,
+rows_processed,
+command_type,
+optimizer_mode,
+action,
+serializable_aborts,
+cpu_time,
+elapsed_time,
+last_active_time,
+last_load_time
+from v$sqlarea;
 exit
