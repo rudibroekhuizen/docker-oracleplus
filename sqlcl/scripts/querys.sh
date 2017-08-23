ecb81789751905bd9995ecf935a3cabcb65014af
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-08-23 10:53:25 +0200

    Update querys.sh

--- sqlcl/scripts/querys.sh
+++ sqlcl/scripts/querys.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 while :
 do
-sql -s sys/manager@oracle:1521/ORCLCDB as sysdba @/scripts/sqlarea.sql | jq -c '.results[].items[]' >> /tmp/sqlarea.json
+sql -s sys/manager@oracle:1521/ORCLCDB as sysdba @/scripts/sqlarea.sql > tmp.json &&
+jq -c '.results[].items[]' tmp.json >> /tmp/sqlarea.json
 sleep 60
 date
 done
