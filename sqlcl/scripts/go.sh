3f37719a7ef203e77611662a03a9bfe7a025a2d4
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-08-25 22:34:42 +0200

    Update go.sh

--- sqlcl/scripts/go.sh
+++ sqlcl/scripts/go.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sql sys/manager@oracle:1521/ORCLCDB as sysdba @/scripts/go.sql
-yes "jq -c '.results[].items[]' /scripts/spool.log" | head -n 60 | parallel -j1 --delay 60 > /tmp/sqlarea.json
+
+
+# yes "jq -c '.results[].items[]' /scripts/spool.log" | head -n 60 | parallel -j1 --delay 60 > /tmp/sqlarea.json
